a68da8e295b65bfeb8dd8373bacd20937570630f
yunst <yunstone75@gmail.com>
2020-07-12 18:56:37 +0900

    //test

---
+++ app/src/main/java/apps/com/kgs/MainActivity.java
@@ -0,0 +1,16 @@
+package apps.com.kgs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ///테스트로 이걸 짜는데 이게 맞음?
+        
+    }
+}
